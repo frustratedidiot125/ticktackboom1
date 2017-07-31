d60fdc6b59370683a7d31d3c7f910c6c38da60cf
frustratedidiot125 <30579982+frustratedidiot125@users.noreply.github.com>
2017-07-31 00:22:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 var alexa = require('alexa-app');
+var express = require("express");
+var PORT = process.env.PORT || 8080;
+var app = express();
+alexaApp.express({
+  expressApp: app,
+  //router: express.Router(),
 
+  // verifies requests come from amazon alexa. Must be enabled for production.
+  // You can disable this if you're running a dev environment and want to POST
+  // things to test behavior. enabled by default.
+  checkCert: true,
+
+  // sets up a GET route when set to true. This is handy for testing in
+  // development, but not recommended for production. disabled by default
+  debug: false
+});
+
+// now POST calls to /test in express will be handled by the app.request() function
+
+// from here on you can setup any other express routes or middlewares as normal
+app.set("view engine", "ejs");
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
 
 // Define an alexa-app
-var app = new alexa.app('number_guessing_game');
-app.id = require('./package.json').alexa.applicationId;
+//var app = new alexa.app('number_guessing_game');
+//app.id = require('./package.json').alexa.applicationId;
 
 app.launch(function(req, res) {
   var number = Math.floor(Math.random() * 99) + 1;
@@ -40,4 +60,38 @@ app.intent('GuessIntent', {
   }
 );
 
-module.exports = app;
+//module.exports = app;
+
+alexaApp.intent("AMAZON.StopIntent", {
+  "slots": {} },
+//"utterances": [ 
+ //              "help", "help me"
+  //              ]
+//  },
+  function(request, response) {
+    response.say("Ok, Goodbye!").shouldEndSession(true);
+  }
+ );
+
+alexaApp.intent("AMAZON.CancelIntent", {
+  "slots": {} },
+//"utterances": [ 
+ //              "help", "help me"
+  //              ]
+//  },
+  function(request, response) {
+    response.say("Cancelling. Goodbye!").shouldEndSession(true);
+  }
+ );
+alexaApp.intent("AMAZON.HelpIntent", {
+  "slots": {} },
+//"utterances": [ 
+ //              "help", "help me"
+  //              ]
+//  },
+  function(request, response) {
+    response.say('Follow the frequent prompts, or say stop at any time to exit.').shouldEndSession(false);
+  }
+ );
+
+app.listen(PORT, () => console.log("Listening on port " + PORT + "."));
