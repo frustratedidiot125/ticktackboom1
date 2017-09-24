61e2e7c2903220864b5df7815d0b022a16c6d9f5
frustratedidiot125 <30579982+frustratedidiot125@users.noreply.github.com>
2017-09-24 18:55:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,47 +45,38 @@ alexaApp.launch(function(req, res) {
     res.shouldEndSession(true);
 });
 
-alexaApp.intent('Generate', {
-    "slots": { "digits": "numba" },
+alexaApp.intent('CurrTime', {
+    "slots": { },
     "utterances": ["{1-100|guess}"]
   },
   function(req, res) {
-    var rnumber = Math.floor(Math.random()*(50-8+1)+8);
+   var ctime = M2M().getTime();
+ var prompt = "The present time on the doomsday clock is " + ctime + ".";
 
-    var rawdigits = req.slot('digits');
-    // Need yo process rawdigits into prpcrssef digit(s), but also check if valid, damaged, garbage, purposrful jigrry pokery, ..how parse? Parse integer? Convert typento numbrr? Handling form inpit?
-   var digits = Number(rawdigits);
-
-  if (!rawdigits){
-    var password = generatePassword(rnumber);
-    res.say('Your new ' + rnumber + ' character password is <prosody rate="x-slow"><say-as interpret-as="spell-out">' + password + '</say-as></prosody>.');
-    res.card({
+  res.say(prompt);
+  res.card({
   type: "Simple",
-  title: "Your New Password", // this is not required for type Simple
-  content: password
+  title: "Doomsday Clock: Current Time", // this is not required for type Simple
+  content: ctime
 });
     res.shouldEndSession(true);
-    
-  } else if (isNaN(digits)){
-    res.say("Sorry, what I heard was " + rawdigits + ". What I need is a proper number. Please try again.").shouldEndSession(false);
-  } else if (rawdigits == "0"){
-    res.say("A zero length password wouldn't be very useful. Please try again.").shouldEndSession(false);
-  } else if (digits < 0){
-    res.say("A password of negative length would be imaginary, or maybe break the laws of physics. Please try again.").shouldEndSession(false);
-  } else if (!Number.isInteger(digits)){
-    res.say("I can't produce only parts of a character. And if i could, the resultant password wouldn't likely be compatible with, anything. Please try again, and stick to integers.").shouldEndSession(false);
-  } else if (digits > 0 && Number.isInteger(digits)){
-    var password = generatePassword(digits);
-    res.say('Your new ' + digits + ' character password is <prosody rate="x-slow"><say-as interpret-as="spell-out">' + password + '</say-as></prosody>.');
-    res.card({
+});
+
+alexaApp.intent('MinToMid', {
+    "slots": { },
+    "utterances": ["{1-100|guess}"]
+  },
+  function(req, res) {
+   var mtime = M2M().get();
+ var prompt = "It is currently " + mtime + "minutes to midnight.";
+
+  res.say(prompt);
+  res.card({
   type: "Simple",
-  title: "Your New Password", // this is not required for type Simple
-  content: password
+  title: "Doomsday Clock: Minutes til midnight", // this is not required for type Simple
+  content: mtime
 });
     res.shouldEndSession(true);
-  } else {
-    res.say("I'm sorry, I think you tried to specify a password length, but I couldn't understand you. Please try again.").shouldEndSession(false);
-  }
 });
 
 //module.exports = app;
@@ -118,7 +109,7 @@ alexaApp.intent("AMAZON.HelpIntent", {
   //              ]
 //  },
   function(request, response) {
-    response.say(prompt + ' Follow any prompts, or say stop at any time to exit.').shouldEndSession(false);
+    response.say('you can say what\'s the current time? or how many minutes until midnight? or say stop at any time to exit.').shouldEndSession(false);
   }
  );
 
