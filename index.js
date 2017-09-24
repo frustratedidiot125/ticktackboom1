b8ba8b4195c841bbb43b6d90f2648da77b716db9
frustratedidiot125 <30579982+frustratedidiot125@users.noreply.github.com>
2017-09-24 19:36:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ module.change_code = 1;
 
 alexaApp.launch(function(req, res) {
   
- var ctime = ; // M2M().getTime();
+ var ctime = // M2M().getTime();
  console.log("getTimeLaunch: " + ctime);
   var prompt = "The current time on the doomsday clock is " + ctime + ". Have a nice day!";
 
@@ -51,7 +51,7 @@ alexaApp.intent('CurrTime', {
     "utterances": ["{1-100|guess}"]
   },
   function(req, res) {
-   var ctime = ; // M2M().getTime();
+   var ctime = // M2M().getTime();
   console.log("getTimeintent: " + ctime);
  var prompt = "The doomsday clock reads " + ctime + ".";
 
@@ -69,7 +69,7 @@ alexaApp.intent('MinToMid', {
     "utterances": ["{1-100|guess}"]
   },
   function(req, res) {
-   var mtime = ; // M2M().get();
+   var mtime = // M2M().get();
   console.log("getminutestil: " + mtime);
  var prompt = "It is currently " + mtime + "minutes to midnight.";
 
